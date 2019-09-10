4548f78c5f9dc9cfcee49c495f9be74fc88b22ae
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-10 10:38:24 -0500

    login is working

---
+++ src/components/animal/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+
+class Dashboard extends Component {
+    render () {
+        return <h2>This will be the Animal Dashboard</h2>
+    }
+}
+
+
+export default Dashboard
