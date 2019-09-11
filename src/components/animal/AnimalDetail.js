e9a8fd52b59164e01bad17ab9dae3cbb522c77cf
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-11 09:59:40 -0500

    populating Animal Detail

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -42,14 +42,31 @@ class AnimalDetail extends Component {
         return (
             <React.Fragment>
                 <h3>Inside Animal Detail</h3>
+                <div>
+                    <h2>Name: <span>{this.state.name}</span></h2>
+                    <h2>Breed: <span>{this.state.breed}</span></h2>
+                    <h2>ADGA Number: <span>{this.state.number}</span></h2>
+                    <h2>Gender: <span>{this.state.gender}</span></h2>
+                    <h2>DOB: <span>{this.state.dateOfBirth}</span></h2>
+                    <h2>Age: <span></span></h2>
+                    <h2>Description: <span>{this.state.description}</span></h2>
+                    <h2>Sire: <span>{this.state.sire}</span></h2>
+                    <h2>Dam: <span>{this.state.dam}</span></h2>
+                </div>
+                <section className="AnimalDetailButton">
+                    <button type="button"
+                    className="EditButton"
+                    onClick = {() => {}}>
+                    Edit
+                    </button>
+                    <button type="button"
+                    className="BackToDashboardButton"
+                    onClick = {() => {}}>
+                    Dashboard
+                    </button>
+                </section>
             </React.Fragment>
         )
-
-
-
-
-
-
     }
 }
 
