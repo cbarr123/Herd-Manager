2aaaf7b21499acb60648cff10a3ba5bce6499911
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-12 10:59:22 -0500

    adding AnimalAdd

--- src/components/animal/Manager.js
+++ src/components/animal/Manager.js
@@ -6,12 +6,12 @@ class Manager extends Component {
         return(
             <React.Fragment>
                 <h3>Manage Herd</h3>
+                <Link to={`/AnimalAdd`}>    
                     <button
-                    type="button"
-                    onClick={this.addAnimal}>
+                    type="button">
                     Add Animal
                     </button>
-
+                </Link>
                     <button
                     type="button"
                     onClick={this.editHerd}>
