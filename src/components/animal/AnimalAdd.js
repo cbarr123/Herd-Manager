46182df97477295caeec05ad0956898fb6528b31
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-12 10:19:23 -0500

    Adding AnimalAdd

--- src/components/animal/AnimalAdd.js
+++ src/components/animal/AnimalAdd.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class AnimalAdd extends Component {
-
-
-
-
-
     
+    
+    
+    render () {
+        return(
+            <React.Fragment>
+
+
+
+            </React.Fragment>
+        )
+    }
 }
