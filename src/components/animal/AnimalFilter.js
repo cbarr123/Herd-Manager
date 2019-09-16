a608536e768eaefc6bac134ec01c00edaada6b9d
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-16 14:44:30 -0500

    added basic structure of the filter view

---
+++ src/components/animal/AnimalFilter.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class AnimalFilter extends Component {
+
+    render () {
+        return (
+            <React.Fragment>
+
+
+
+                
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default AnimalFilter
