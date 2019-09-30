6ad69d66e29bdde9eb84105086799f56f1c3a348
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-09-30 11:25:16 -0500

    added status to card

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -15,6 +15,7 @@ class AnimalCard extends Component {
                         <CardTitle>Name: <span>{this.props.animal.name}</span></CardTitle>
                         <CardSubtitle>Breed: <span>{this.props.animal.breed}</span></CardSubtitle>
                         <CardText>DOB: <span>{this.props.animal.dateOfBirth}</span></CardText>
+                        <CardText>Status: <span>{this.props.animal.status}</span></CardText>
                         <Button className="Button-Input">View Detail</Button>
                     </CardBody>
                 </Card>
